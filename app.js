89ff3cbd17f00f74abc0d5acfc82f8436e49019e
yakan <enomatome@gmail.com>
2020-11-23 22:30:27 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 'use strict';
 
- const memo = new Map();
- memo.set(0,0);
- memo.set(1,1);
-
 function fib(n){
-    if(memo.has(n)){
-        return memo.get(n);
+    if (n==0){
+        return 0;
+    }else if(n == 1){
+        return 1;
     }
-    const value = fib(n - 1) + fib(n - 2);
-    memo.set(n,value);
-    return value;
 
-    }
-const length = 40;
-for(let i =0;i <= length; i++){
-    console.log(fib(i));
+    return fib(n-1) + fib(n-2);
+    
+}
+
+for (let i = 0; i <= 40 ; i++){
+    console.log( i + "のフィボナッチ数は" + fib(i) + "です");
+    
 }
