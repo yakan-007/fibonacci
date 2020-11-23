4bc3da3875fe83c3936c8f6f28ad94d68518a258
yakan <enomatome@gmail.com>
2020-11-23 22:53:54 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
 
-function fib(n){
-    if (n==0){
-        return 0;
-    }else if(n == 1){
-        return 1;
-    }
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 
-    return fib(n-1) + fib(n-2);
-    
+
+function fib(n){
+    if (memo.has(n)){
+        return memo.get(n);
+
+    }else{
+        let value = fib(n-1) + fib(n-2);
+        memo.set(n, value);
+        return value;
+    }
 }
+   
 
 for (let i = 0; i <= 40 ; i++){
     console.log( i + "のフィボナッチ数は" + fib(i) + "です");
